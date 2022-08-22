87a333793186efb5c1bd4daa65c7617d7631e322
konjikun <107373499+konjikun@users.noreply.github.com>
2022-08-23 03:43:05 +0900

    fadeOutまで

--- test.js
+++ test.js
@@ -4,12 +4,13 @@ addButton.classList.add('addition');
 addButton.type = 'button';
 addButton.value = 'serviceXから入力';
 addButton.id = 'serviceX';
-document.querySelector('#vi > p').appendChild(addButton);//ボタンを置きたい場所
+document.querySelector('#vi > p').appendChild(addButton);//ボタンを置きたい場所を指定してください・・・１
+
 
 
 //function aiueo{}
 //iframe生成
-$('#serviceX').on('click',function(){
+
 const modalArea= document.createElement('section')
 modalArea.style.width='100%';
 modalArea.style.height='100%'
@@ -18,55 +19,82 @@ modalArea.style.left='0'
 modalArea.style.position='fixed'
 modalArea.style.background ='rgba(0,0,0,50%)'
 modalArea.id='modalA'
+modalArea.style.display ='none'
+
 
 const ifra = document.createElement('iframe');
 ifra.src='https://konjikun.github.io/administraition-form/'
 ifra.id='iframe-body'
 ifra.style.width='600px'
 ifra.style.height='800px'
+ifra.style.backgroundAttachment='fixed'
 ifra.style.top='50%'
 ifra.style.left='50%'
 ifra.style.backgroundColor='white'
 ifra.style.border='none'
 ifra.style.position='absolute'
 ifra.style.transform='translate(-50%,-50%)'
+ifra.style.display='none'
+
+/*
+$('body').css({
+	position:'fixed',
+})
+*/
 
 document.querySelector('#contents').appendChild(modalArea)
 document.querySelector('#contents').appendChild(ifra);
 
+$('#serviceX').on('click',function(){
+	$('#iframe-body').fadeIn();
+	$('#modalA').fadeIn();
+	s =$('iframe').offset().top;
+$(window).scrollTop(s)
+})
 
-//ここに欲しいデータの名前を打ち込んでください！
+
+//ここに欲しいデータの名前を打ち込んでください！ *呼び名は決まっています。・・・２
 //ex.  'name':'',
 demandData = [
     'nameKatakana',
     'nameKanji',
     'addres',
     'email',
-    'phone',
     'phoneAdvance',
     'phoneCenter',
     'phoneBack',
 ]
 //JSON.parseの文字列はダブルクオーテーションで囲まないとエラーになる。
-demandData = JSON.parse(JSON.stringify(demandData));
 
-//必要データを要求
-window.addEventListener('message',()=>{
+
+
+const readyIframe=()=>{
+	demandData = JSON.parse(JSON.stringify(demandData));
 	prottype=['0','1']
 	console.log(demandData)
 	$('#iframe-body')[0].contentWindow.postMessage(demandData, '*')
-	window.removeEventListener('message',()=>{
-		prottype=['0','1']
-		console.log(demandData)
-		$('#iframe-body')[0].contentWindow.postMessage(demandData, '*')
-	})
-})
+}
+//必要データを要求
+window.addEventListener('message',readyIframe,{once:true})
 
+
+//ここでフォームのIDを取得をしてください *２番で打ち込んだ順に記入してください。
 window.addEventListener('message',(serve)=>{
-    serveData = serve.data
+	
+if (typeof(serve.data) !=='string'){
+	$('#iframe-body').fadeOut();
+	$('#modalA').fadeOut();
+	console.log(typeof(serve.data))
+	serveData = serve.data
 	console.log(serveData)
-})
-
+	document.getElementById('moushikomiShimeiKn').value = serveData[0]
+    document.getElementById('moushikomiShimeiKj').value = serveData[1]
+    document.getElementById('moushikomiMail').value = serveData[2]
+    document.getElementById('confirmMoushikomiMail').value = serveData[3]
+    document.getElementById('moushikomiPhoneNo1').value = serveData[4]
+    document.getElementById('moushikomiPhoneNo2').value = serveData[5]
+    document.getElementById('moushikomiPhoneNo3').value = serveData[6]
+}
 })
 
 
@@ -160,19 +188,19 @@ bodyId.id='body'
 
 //反応してない！！
 $('#iframe').css({
-  'position':'fixed',
-  'top':0,
-  'left':200,
-  'width':'100%',
-  'height':'100%',
-  'text-align':'center',
-  'background-color':'rgba(0,0,0,5)',
+  position:'fixed',
+  top:0,
+  left:200,
+  width:'100%',
+  height:'100%',
+  textalign:'center',
+  backgroundcolor:'rgba(0,0,0,5)',
   //paddingのみ抜き
-  'overflow':'auto',
-  'opacity':0,
-  'visibility':'hide',
-  'transition':'.3s',
-  'box-sizing':'border-box'
+  overflow:'auto',
+  opacity:0,
+  visibility:'hide',
+  transition:'.3s',
+  boxsizing:'border-box'
 });
 
 
@@ -267,7 +295,7 @@ $(function(){
 }
 */
 //iframe埋め込み  
-  ('#iframe').fadein();
+
 document.querySelector('contents').appendChild(ifra)
 
 (document.getElementById('serviceX')).click(function(){
